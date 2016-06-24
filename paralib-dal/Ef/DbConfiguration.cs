3a988a46b3564fce2018addd7d36ed867f49ec3a
Joe Barnett <joebarn@users.noreply.github.com>
2016-06-23 20:51:47 -0400

    'ef support'

---
+++ paralib-dal/Ef/DbConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NET=System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using com.paralib;
+
+namespace com.paralib.Dal.Ef
+{
+    public class DbConfiguration : NET.DbConfiguration
+    {
+        public DbConfiguration()
+        {
+            SetExecutionStrategy("System.Data.SqlClient", () => new DefaultExecutionStrategy());
+            SetDefaultConnectionFactory(new SqlConnectionFactory(Paralib.Configuration.Dal.ConnectionString));
+        }
+    }
+}
---
+++ paralib-dal/Ef/DbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Reflection;
+using NET=System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Configuration;
+using System.Text.RegularExpressions;
+
+namespace com.paralib.Dal.Ef
+{
+    public class DbContext : NET.DbContext
+    {
+
+        public DbContext(): base(Paralib.Configuration.ConnectionString)
+        {
+            NET.Database.SetInitializer<DbContext>(null);
+        }
+
+
+        protected override void OnModelCreating(NET.DbModelBuilder modelBuilder)
+        {
+            // Configure Code First to ignore PluralizingTableName convention
+            // If you keep this convention, the generated tables 
+            // will have pluralized names.
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            var c = modelBuilder.Conventions;
+
+            modelBuilder.Types()
+            .Configure(config => config.ToTable(GetTableName(config)));
+
+
+            modelBuilder.Properties()
+            .Configure(config => config.HasColumnName(GetColumnName(config.ClrPropertyInfo)));
+
+        }
+
+        private static string GetTableName(ConventionTypeConfiguration config)
+        {
+            var result = Regex.Replace(config.ClrType.Name, ".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+            return result.ToLower() + "s";
+        }
+
+
+        private static string GetColumnName(PropertyInfo property)
+        {
+            var result = Regex.Replace(property.Name, ".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+            //var result = property.DeclaringType.Name + "_";
+            //result += Regex.Replace(property.Name,".[A-Z]", m => m.Value[0] + "_" + m.Value[1]);
+            return result.ToLower();
+        }
+
+
+    }
+}
