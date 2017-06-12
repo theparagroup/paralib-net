36a7030d3dd4ed412b3ff75e781bd4b060fcabd5
Joe Barnett <joebarn@users.noreply.github.com>
2017-06-12 02:42:16 -0400

    '-m'

--- paralib-migrations/CodeGen/EfContextGenerator.cs
+++ paralib-migrations/CodeGen/EfContextGenerator.cs
@@ -25,22 +25,35 @@ namespace com.paralib.Migrations.CodeGen
             WriteLine("using System;");
             WriteLine("using System.Data.Entity;");
             WriteLine("using com.paralib.Dal.Ef;");
+            WriteLine("using para=com.paralib.Ado;");
             WriteLine();
 
             if (ClassOptions.Namespace != null) WriteLine($"namespace {ClassOptions.Namespace}{(ClassOptions.SubNamespace != null ? "." + ClassOptions.SubNamespace : "")}\n{{");
 
             WriteLine("\t[DbConfigurationType(typeof(EfConfiguration))]");
+
             WriteLine($"\tpublic partial class DbContext:EfContext");
             WriteLine("\t{");
 
-            //diagnostics
             WriteLine();
-            WriteLine("#if DEBUG");
-            WriteLine("\t\tpublic DbContext()");
+            WriteLine("\t\tpublic DbContext() { _init(); }");
+            WriteLine("\t\tpublic DbContext(string connectionString) : base(connectionString) { _init(); }");
+            WriteLine("\t\tpublic DbContext(para.Database database) : base(database) { _init(); }");
+            WriteLine();
+
+            WriteLine("\t\tprivate void _init()");
             WriteLine("\t\t{");
-            WriteLine("\t\t\tDatabase.Log = message => System.Diagnostics.Debug.WriteLine(message);");
+            WriteLine("\t\t\t OnInit();");
             WriteLine("\t\t}");
-            WriteLine("#endif");
+            WriteLine();
+
+            //diagnostics
+            WriteLine("\t\tprotected virtual void OnInit()");
+            WriteLine("\t\t{");
+            WriteLine("\t\t\t#if DEBUG");
+            WriteLine("\t\t\tDatabase.Log = message => System.Diagnostics.Debug.WriteLine(message);");
+            WriteLine("\t\t\t#endif");
+            WriteLine("\t\t}");
             WriteLine();
 
             Table[] tables = null;
