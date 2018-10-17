6a648e0114613942611ab2a4042d960f159f9226
Joe Barnett <joebarn@users.noreply.github.com>
2018-10-16 23:14:41 -0400

    adding timeout to forms authentication api

--- paralib-mvc/Authentication/Forms.cs
+++ paralib-mvc/Authentication/Forms.cs
@@ -8,18 +8,24 @@ namespace com.paralib.Mvc.Authentication
 {
     public static class Forms
     {
+        /*
+
+        note: not sure why this method was originally provided
+        
         public static void Authenticate(string userName)
         {
             NET.FormsAuthentication.RedirectFromLoginPage(userName, false);
         }
 
-        public static void Authenticate(ParaPrinciple principle, string defaultUrl = null)
+        */
+
+        public static void Authenticate(ParaPrinciple principle, string defaultUrl = null, int? timeout = null)
         {
             string data = Json.Serialize(principle.Roles);
-            Authenticate(principle?.Identity?.Name, data, defaultUrl);
+            Authenticate(principle?.Identity?.Name, data, defaultUrl, timeout);
         }
 
-        public static void Authenticate(string userName, string data, string defaultUrl = null)
+        public static void Authenticate(string userName, string data, string defaultUrl = null, int? timeout=null)
         {
             //TODO make this an option
             bool persistCookie = false;
@@ -27,7 +33,7 @@ namespace com.paralib.Mvc.Authentication
             //Note: FormsAuthentication.SlidingExpiration defaults to true, so the expiration will reset when more than half the time has expired
 
             //create ticket and encrypt
-            NET.FormsAuthenticationTicket ticket= new NET.FormsAuthenticationTicket(1, userName, DateTime.Now, DateTime.Now.AddMinutes(Paralib.Mvc.Authentication.Timeout), persistCookie, data);
+            NET.FormsAuthenticationTicket ticket= new NET.FormsAuthenticationTicket(1, userName, DateTime.Now, DateTime.Now.AddMinutes(timeout ?? Paralib.Mvc.Authentication.Timeout), persistCookie, data);
             string encryptedTicket = NET.FormsAuthentication.Encrypt(ticket);
 
             //save cookie
