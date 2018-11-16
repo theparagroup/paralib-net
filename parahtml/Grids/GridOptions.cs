1a7203c9c241bf70cca59d495ea03582d71fed7e
Joe Barnett <joebarn@users.noreply.github.com>
2018-11-15 23:56:01 -0500

    '.'

--- parahtml/Grids/GridOptions.cs
+++ parahtml/Grids/GridOptions.cs
@@ -8,9 +8,9 @@ namespace com.parahtml.Grids
 {
     public class GridOptions
     {
-        public string[] ColumnClassList { set; get; }
         public string ContainerClass { set; get; }
         public string RowClass { set; get; }
         public string ColumnClass { set; get; }
+        public string[] ColumnClassList { set; get; }
     }
 }
