d1a3dd790c29b90eedc1b5544783aad709dc7a3b
Joe Barnett <joebarn@users.noreply.github.com>
2019-01-14 12:34:13 -0500

    'adding packages folder - it was ignored'

---
+++ parahtml/Packages/Package.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.parahtml.Packages
+{
+    /*
+
+
+
+    */
+
+    public abstract class Package
+    {
+
+        public virtual List<Dependency> Dependencies
+        {
+            get
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
